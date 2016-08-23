61b124fd33f65690237f8536106df41b0ce43251
Thiti Yamsung <thiti.59h@gmail.com>
2016-08-23 22:46:31 +0700

    First commit.
    Signed-off-by: Thiti Yamsung <thiti.59h@gmail.com>

---
+++ ICloneableExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICloneableExample {
+    class Program {
+        static void Main(string[] args) {
+            myNumber obj1, obj2;
+
+            obj1 = new myNumber(1);
+            obj2 = (myNumber)obj1.Clone();
+
+            obj1.number = 90;
+
+            Console.WriteLine("obj1 number: " + obj1.number.ToString());
+            Console.WriteLine("obj2 number: " + obj2.number.ToString());
+
+            Console.ReadKey();
+        }
+    }
+
+    class myNumber : ICloneable {
+
+        private int _number = 0;
+
+        public int number { get { return _number; } set { _number = value; } }
+
+        public myNumber(int inNumber) {
+            _number = inNumber;
+        }
+
+        public object Clone() {
+            return new myNumber(_number);
+        }
+    }
+}
